275e0fb6545830cca76d0390d972208f3d735723
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2023-01-19 16:02:18 -0300

    First commit :tada:

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className='bg-teal-800 w-auto text-white rounded m-2 p-4'>
+      <h1>Hello Vitejs</h1>
+      <h1>Hello Vitejs</h1>
+      <h1>Hello Vitejs</h1>
+    </div>
+  )
+}
+
+export default App
