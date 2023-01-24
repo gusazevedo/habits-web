6f61b0dd25a97374a707cd40ccede19c02956e46
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2023-01-24 15:09:16 -0300

    Finaliza tela de listagem de habitos

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,5 +1,6 @@
 import * as Popover from '@radix-ui/react-popover';
 import {ProgressBar} from './ProgressBar';
+import clsx from 'clsx';
 
 interface HabitDayProps {
     completed: number;
@@ -12,7 +13,15 @@ export function HabitDay({amount, completed}: HabitDayProps) {
     return (
         <Popover.Root>
             <Popover.Trigger
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"/>
+                className={clsx('w-10 h-10 border-2 rounded-lg', {
+                    'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+                    'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
+                    'bg-violet-800 border-zinc-600': completedPercentage >= 20 && completedPercentage < 40,
+                    'bg-violet-700 border-zinc-500': completedPercentage >= 40 && completedPercentage < 60,
+                    'bg-violet-600 border-zinc-500': completedPercentage >= 60 && completedPercentage < 80,
+                    'bg-violet-500 border-zinc-400': completedPercentage >= 80,
+                })}
+            />
 
             <Popover.Portal>
                 <Popover.Content
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,5 +1,5 @@
-import {HabitDay} from "./HabitDay";
-import {generateYearDays} from "../utils/functions";
+import {HabitDay} from './HabitDay';
+import {generateYearDays} from '../utils/functions';
 
 const week = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const summaryDates = generateYearDays();
@@ -8,27 +8,31 @@ const amountOfDaysToFill = minimumDaysSize - summaryDates.length;
 
 export function SummaryTable() {
     return (
-        <div className='w-full flex'>
-            <div className='grid grid-rows-7 grid-flow-row gap-3'>
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
                 {week.map((day, index) => (
                     <div
                         key={index}
-                        className='text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center'
+                        className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
                     >
                         {day}
                     </div>
                 ))}
             </div>
 
-            <div className='grid grid-rows-7 grid-flow-col gap-3'>
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(day => (
-                    <HabitDay key={day.toString()}/>
+                    <HabitDay
+                        key={day.toString()}
+                        amount={5}
+                        completed={Math.round(Math.random() * 5)}
+                    />
                 ))}
                 {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill})
                     .map((_, index) => (
                         <div
                             key={index}
-                            className='w-10 h10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'
+                            className="w-10 h10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
                         />
                     ))
                 }
